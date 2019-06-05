44062942827d45312faaa79d8cc28309769fa47b
henrievjen <46657460+henrievjen@users.noreply.github.com>
2019-06-04 21:33:22 -0500

    setting up GitHub

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Profile } from './github.api.interface';
 import { catchError } from 'rxjs/operators';
