9b32c3ce3926131ff44bbdab47ef695e66082b70
henrievjen <46657460+henrievjen@users.noreply.github.com>
2019-07-15 13:59:28 -0500

    'Page Not Found' page working

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,13 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
     sessionStorage.clear();
     document.getElementById('home-navbar').classList.remove('active');
     document.getElementById('profile-navbar').classList.remove('active');
     document.getElementById('repositories-navbar').classList.remove('active');
+
+    (<HTMLInputElement> document.getElementById('navbar-username')).placeholder = 'Search Users';
+  }
+
+  public validation(): void {
+    if ((<HTMLInputElement> document.getElementById('username')).value.trim().length > 0) {
+      this.appService.setUser((<HTMLInputElement> document.getElementById('username')).value);
+      sessionStorage.setItem('username', (<HTMLInputElement> document.getElementById('username')).value);
+      (<HTMLInputElement> document.getElementById('username')).value = "";
+    }
   }
 
 }
